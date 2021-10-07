1282d4f4e5f4118ae8870962efade46a92c9390e
nicktecno <nick.tecno@gmail.com>
2021-10-07 16:42:53 -0300

    update: Aplication acomplished

---
+++ src/components/Map/test.tsx
@@ -0,0 +1,40 @@
+import { render, screen } from '@testing-library/react'
+
+import Map from '.'
+
+describe('<Map />', () => {
+  it('should render without any marker', () => {
+    render(<Map />)
+
+    expect(
+      screen.getByRole('link', {
+        name: /a js library for interactive maps/i
+      })
+    ).toBeInTheDocument()
+  })
+
+  it('should render with the marker in correct place', () => {
+    const place = {
+      id: '1',
+      name: 'Petrópolis',
+      slug: 'petropolis',
+      location: {
+        latitude: 0,
+        longitude: 0
+      }
+    }
+    const place2 = {
+      id: '2',
+      name: 'Reykjavik',
+      slug: 'reykjavik',
+      location: {
+        latitude: 129,
+        longitude: -50
+      }
+    }
+    render(<Map places={[place, place2]} />)
+
+    expect(screen.getByTitle(/petrópolis/i)).toBeInTheDocument()
+    expect(screen.getByTitle(/reykjavik/i)).toBeInTheDocument()
+  })
+})
---
+++ src/pages/place/[slug].tsx
@@ -0,0 +1,54 @@
+import client from 'graphql/client'
+import {
+  GetPageBySlugQuery,
+  GetPlaceBySlugQuery,
+  GetPlacesQuery
+} from 'graphql/generated/graphql'
+import {
+  GET_PAGE_BY_SLUG,
+  GET_PLACES,
+  GET_PLACE_BY_SLUG
+} from 'graphql/queries'
+import { GetStaticProps } from 'next'
+import { useRouter } from 'next/dist/client/router'
+import PlacesTemplate, { PlacesTemplateProps } from 'templates/Places'
+
+export default function Place({ place }: PlacesTemplateProps) {
+  const router = useRouter()
+
+  if (router.isFallback) return null
+
+  return <PlacesTemplate place={place} />
+}
+
+export const getStaticPaths = async () => {
+  const { places } = await client.request<GetPlacesQuery>(GET_PLACES, {
+    first: 3
+  })
+
+  const paths = places.map(({ slug }) => ({
+    params: { slug }
+  }))
+
+  return {
+    paths,
+    fallback: true
+  }
+}
+
+export const getStaticProps: GetStaticProps = async ({ params }) => {
+  const { place } = await client.request<GetPlaceBySlugQuery>(
+    GET_PLACE_BY_SLUG,
+    {
+      slug: `${params?.slug}`
+    }
+  )
+  if (!place) return { notFound: true }
+
+  return {
+    revalidate: 5,
+    props: {
+      place
+    }
+  }
+}
